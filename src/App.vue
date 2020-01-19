1958371c8f3f8fe98190707d51d3545b75cf18c7
Aaron Yuan <aaronyuan27@gmail.com>
2020-01-19 01:24:49 -0700

    display single quote

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="container">
-    
+        <appQuoteGrid :quotes="quotes"></appQuoteGrid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from "./components/QuoteGrid.vue";
     export default {
-        
+        data: function(){
+           return{
+               quotes:[
+                   "Just a Quote to see something"
+               ],
+               maxQuotes: 10,
+               
+           } 
+        },
+        components:{
+            appQuoteGrid: QuoteGrid
+        }
     }
 </script>
 
